0271f5fcc6e73343e07b1c474388313db6f1da21
JavsShao <javs_shao@163.com>
2018-07-10 13:29:22 +0800

    判断账号是否存在之序列化器

---
+++ meiduo_mall/meiduo_mall/apps/users/serializers.py
@@ -0,0 +1,74 @@
+from django_redis import get_redis_connection
+from rest_framework import serializers
+import re
+
+from .models import User
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    """创建用户的序列化器"""
+    password2 = serializers.CharField(label='确认密码',write_only=True)
+    sms_code = serializers.CharField(label='短信验证码',write_only=True)
+    allow = serializers.CharField(label='同意协议',write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'password2', 'sms_code', 'mobile', 'allow')
+        extra_kwargs = {
+            'username': {
+                'min_length': 5,
+                'max_length': 20,
+                'error_messages': {
+                    'min_length': '仅允许5-20个字符的用户名',
+                    'max_length': '仅允许5-20个字符的用户名',
+                }
+            },
+            'password': {
+                'write_only': True,
+                'min_length': 8,
+                'max_length': 20,
+                'error_messages': {
+                    'min_length': '仅允许8-20个字符的密码',
+                    'max_length': '仅允许8-20个字符的密码',
+                }
+            }
+        }
+
+    def validate_mobile(self, value):
+        '''验证手机号'''
+        if not re.match(r'^1[3-9]\d{9}$',value):
+            raise serializers.ValidationError('手机号格式错误')
+        return value
+
+    def validate_allow(self, value):
+        """检验用户是否同意协议"""
+        if value != 'True':
+            raise serializers.ValidationError('请同意用户协议')
+        return value
+
+    def validate(self, data):
+        """判断两次密码是否相同"""
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError('两次密码不一致')
+
+        # 判断短信验证码
+        redis_conn = get_redis_connection('verify_codes')
+        mobile = data['mobile']
+        real_sms_code = redis_conn.get('sms_%s' % mobile)
+        if not re.match(r'^1[3-9]\d{9}$', data):
+            raise serializers.ValidationError('手机号格式错误')
+        return data
+
+    def create(self, validated_data):
+        """重写保存方法，增加密码加密"""
+        # 移除数据库模型类中不存在的属性
+        del validated_data['password2']
+        del validated_data['sms_code']
+        del validated_data['allow']
+
+        user = super().create(validated_data)
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -5,3 +5,11 @@ from . import serializers
 
 
 # Create your views here.
+
+class UserView(CreateAPIView):
+    """
+    用户注册
+    传入参数：
+        username,password,password2,sms_code,mobile,allow
+    """
+    serializers_class = serializers.CreateUserSerializer
