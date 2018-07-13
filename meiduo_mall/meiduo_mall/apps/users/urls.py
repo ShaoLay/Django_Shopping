36db29af0ceb4da3b2d0f83618c7e6fca312fce9
JavsShao <javs_shao@163.com>
2018-07-13 10:27:56 +0800

    单账户登录之后端

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from rest_framework_jwt.views import obtain_jwt_token
 
 from . import views
 
 
 urlpatterns = [
     url(r'^users/$',views.UserView.as_view()),
+    url(r'^authorizations/$', obtain_jwt_token),  # 登录认证
 ]
---
+++ meiduo_mall/meiduo_mall/apps/users/utils.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.backends import ModelBackend
+import re
+
+from .models import User
+
+
+def jwt_response_payload_handler(token,user=None,request=None):
+    """自定义jwt认证成功返回数据"""
+    return {
+        'token':token,
+        'user_id':user.id,
+        'username':user.username
+    }
