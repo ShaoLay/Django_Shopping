2c2321bb92ca66dd22c378674f8ee11bb071f57e
JavsShao <javs_shao@163.com>
2018-07-13 11:30:06 +0800

    手机号 & 用户名等多种登录方式

--- meiduo_mall/meiduo_mall/apps/users/utils.py
+++ meiduo_mall/meiduo_mall/apps/users/utils.py
@@ -11,3 +11,35 @@ def jwt_response_payload_handler(token,user=None,request=None):
         'user_id':user.id,
         'username':user.username
     }
+
+
+def get_user_by_account(account):
+    """根据账户获取用户对象"""
+    try:
+        if re.match(r'^1[3-9]\d{9}$',account):
+            # 手机号
+            user = User.objects.get(mobile=account)
+        else:
+            user = User.objects.get(username=account)
+    except User.DoerNotExist:
+        return None
+
+    else:
+        return user
+
+class UsernameMobileAuthBackend(ModelBackend):
+    """
+    自定义用户名或手机号
+    """
+    def authenticate(self,request,username=None,password=None,**kwargs):
+        """
+        :param request:
+        :param username:可能是手机号,也有可能是用户名
+        :param password:
+        :param kwargs:
+        :return:
+        """
+        user = get_user_by_account(username)
+
+        if user is not None and user.check_password(password):
+            return user
--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -3,8 +3,8 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from users.models import User
 from . import serializers
+from .models import User
 # Create your views here.
 
 
@@ -12,37 +12,45 @@ from . import serializers
 class UserView(CreateAPIView):
     """
     用户注册
-
     传入参数：
         username, password, password2, sms_code, mobile, allow
     """
     serializer_class = serializers.CreateUserSerializer
 
 
+# url(r'^usernames/(?P<username>\w{5,20})/count/$', views.UsernameCountView.as_view()),
 class UsernameCountView(APIView):
     """
     用户名数量
     """
-    def get(self,request,username):
-        """获取指定用户数量"""
+    def get(self, request, username):
+        """
+        获取指定用户名数量
+        """
         count = User.objects.filter(username=username).count()
 
         data = {
-            'username':username,
-            'count':count
+            'username': username,
+            'count': count
         }
 
         return Response(data)
 
+
+# url(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
 class MobileCountView(APIView):
-    """手机号数量"""
-    def get(self,request,mobile):
-        """获取指定手机号数量"""
+    """
+    手机号数量
+    """
+    def get(self, request, mobile):
+        """
+        获取指定手机号数量
+        """
         count = User.objects.filter(mobile=mobile).count()
 
         data = {
-            'mobile':mobile,
-            'count':count
+            'mobile': mobile,
+            'count': count
         }
 
         return Response(data)
