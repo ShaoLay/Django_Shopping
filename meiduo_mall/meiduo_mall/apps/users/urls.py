d9d9b27c3aa38b761f0102e3cadb0797c90523be
JavsShao <javs_shao@163.com>
2018-07-13 11:08:10 +0800

    判断用户名 & 手机号是否已存在之前后端实现

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -6,5 +6,7 @@ from . import views
 
 urlpatterns = [
     url(r'^users/$',views.UserView.as_view()),
+url(r'^usernames/(?P<username>\w{5,20})/count/$', views.UsernameCountView.as_view()),
+    url(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
     url(r'^authorizations/$', obtain_jwt_token),  # 登录认证
 ]
--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from users.models import User
 from . import serializers
 # Create your views here.
 
@@ -16,4 +19,30 @@ class UserView(CreateAPIView):
     serializer_class = serializers.CreateUserSerializer
 
 
+class UsernameCountView(APIView):
+    """
+    用户名数量
+    """
+    def get(self,request,username):
+        """获取指定用户数量"""
+        count = User.objects.filter(username=username).count()
 
+        data = {
+            'username':username,
+            'count':count
+        }
+
+        return Response(data)
+
+class MobileCountView(APIView):
+    """手机号数量"""
+    def get(self,request,mobile):
+        """获取指定手机号数量"""
+        count = User.objects.filter(mobile=mobile).count()
+
+        data = {
+            'mobile':mobile,
+            'count':count
+        }
+
+        return Response(data)
