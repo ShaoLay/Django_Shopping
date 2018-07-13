cde91ea5308711eae969b547efb3982ca614ea90
JavsShao <javs_shao@163.com>
2018-07-13 10:14:20 +0800

    签发JWT_token & 修改序列化器

--- meiduo_mall/meiduo_mall/apps/users/serializers.py
+++ meiduo_mall/meiduo_mall/apps/users/serializers.py
@@ -2,6 +2,8 @@ from django_redis import get_redis_connection
 from rest_framework import serializers
 import re
 
+from rest_framework.settings import api_settings
+
 from .models import User
 
 
@@ -10,10 +12,11 @@ class CreateUserSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(label='确认密码', write_only=True)
     sms_code = serializers.CharField(label='短信验证码', write_only=True)
     allow = serializers.CharField(label='同意协议', write_only=True)
+    token = serializers.CharField(label='JWT token',read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'password', 'password2', 'sms_code', 'mobile', 'allow')
+        fields = ('id', 'username', 'password', 'password2', 'sms_code', 'mobile', 'allow','token')
         extra_kwargs = {
             'username': {
                 'min_length': 5,
@@ -77,6 +80,15 @@ class CreateUserSerializer(serializers.ModelSerializer):
 
         user.set_password(validated_data['password'])
         user.save()
+
+        # 签发jwt token
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+
+        user.token = token
 
         return user
 
