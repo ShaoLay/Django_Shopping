fe127ff07fbb774398a3797a5ed795e38a063afd
JavsShao <javs_shao@163.com>
2018-07-22 17:10:17 +0800

    用户地址数据库模型类

--- meiduo_mall/meiduo_mall/apps/users/models.py
+++ meiduo_mall/meiduo_mall/apps/users/models.py
@@ -4,6 +4,8 @@ from itsdangerous import TimedJSONWebSignatureSerializer as TJWSSerializer, BadD
 
 # Create your models here.
 from django.conf import settings
+
+from meiduo_mall.utils.models import BaseModel
 from . import constants
 
 
@@ -44,3 +46,28 @@ class User(AbstractUser):
                 return None
             else:
                 return user
+
+class Address(BaseModel):
+    """
+    用户地址
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses', verbose_name='用户')
+    title = models.CharField(max_length=20, verbose_name='地址名称')
+    receiver = models.CharField(max_length=20, verbose_name='收货人')
+    province = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='province_addresses',
+                                 verbose_name='省')
+    city = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='city_addresses', verbose_name='市')
+    district = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='district_addresses',
+                                 verbose_name='区')
+    place = models.CharField(max_length=50, verbose_name='地址')
+    mobile = models.CharField(max_length=11, verbose_name='手机')
+    tel = models.CharField(max_length=20, null=True, blank=True, default='', verbose_name='固定电话')
+    email = models.CharField(max_length=30, null=True, blank=True, default='', verbose_name='电子邮箱')
+    is_deleted = models.BooleanField(default=False, verbose_name='逻辑删除')
+
+    class Meta:
+        db_table = 'tb_address'
+        verbose_name = '用户地址'
+        verbose_name_plural = verbose_name
+        ordering = ['-update_time']  # 指明默认排序
