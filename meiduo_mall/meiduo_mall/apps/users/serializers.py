46c160577adc5c363e00947ca97c6ecba5dc1f3e
JavsShao <javs_shao@163.com>
2018-07-27 12:56:13 +0800

    用户地址管理后端之序列化器

--- meiduo_mall/meiduo_mall/apps/users/serializers.py
+++ meiduo_mall/meiduo_mall/apps/users/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 import re
 from rest_framework_jwt.settings import api_settings
 
-from .models import User
+from .models import User, Address
 from celery_tasks.email.tasks import send_active_email
 
 
@@ -129,7 +129,44 @@ class EmailSerializer(serializers.ModelSerializer):
         return instance
 
 
+class UserAddressSerializer(serializers.ModelSerializer):
+    """
+    用户地址序列化器
+    """
+    province = serializers.StringRelatedField(read_only=True)
+    city = serializers.StringRelatedField(read_only=True)
+    district = serializers.StringRelatedField(read_only=True)
+    province_id = serializers.IntegerField(label='省ID', required=True)
+    city_id = serializers.IntegerField(label='市ID', required=True)
+    district_id = serializers.IntegerField(label='区ID', required=True)
 
+    class Meta:
+        model = Address
+        exclude = ('user', 'is_deleted', 'create_time', 'update_time')
+
+    def validate_mobile(self, value):
+        """
+        验证手机号
+        """
+        if not re.match(r'^1[3-9]\d{9}$', value):
+            raise serializers.ValidationError('手机号格式错误')
+        return value
+
+    def create(self, validated_data):
+        """
+        保存
+        """
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
+
+
+class AddressTitleSerializer(serializers.ModelSerializer):
+    """
+    地址标题
+    """
+    class Meta:
+        model = Address
+        fields = ('title',)
 
 
 
